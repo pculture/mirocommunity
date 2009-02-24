0a6cb23b24c1d9a11c1ee5a9d2567bf7927dc415
Christopher Allan Webber <cwebber@dustycloud.org>
2009-02-24 15:13:42 -0600

    Added the base template and got enough changes in place to start on video viewing

--- localtv/subsite_urls.py
+++ localtv/subsite_urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns(
     'localtv.views',
-    (r'^$', 'subsite_index', {}, 'localtv_subsite_index'))
+    (r'^$', 'subsite_index', {}, 'localtv_subsite_index'),
+    (r'^video/(?P<video_id>[0-9]+)/$', 'view_video',
+     {}, 'localtv_subsite_view_video'))
--- localtv/views.py
+++ localtv/views.py
@@ -1,7 +1,23 @@
 from django.contrib.sites.models import Site
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
-def subsite_index(request):
-    current_site = Site.objects.get_current()
-    return HttpResponse('subsite: %s' % current_site.name)
+from localtv.decorators import get_sitelocation
+from localtv import models
+
+
+@get_sitelocation
+def subsite_index(request, sitelocation=None):
+    return render_to_response(
+        'localtv/subsite/index.html',
+        {'sitelocation': sitelocation})
+
+
+@get_sitelocation
+def view_video(request, video_id, sitelocation=None):
+    video = get_object_or_404(models.Video, pk=video_id, site=sitelocation.site)
+
+    return render_to_response(
+        'localtv/subsite/view_video.html',
+        {'sitelocation': sitelocation,
+         'current_video': video})
