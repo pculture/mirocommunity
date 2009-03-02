23efa1a95dd062a0645a10e914149f135d6856ba
Christopher Allan Webber <cwebber@dustycloud.org>
2009-03-02 13:58:44 -0600

    Moving the subsite urls to the views directory

--- localtv/subsite_urls.py
+++ localtv/subsite_urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import patterns
 
+from localtv.openid_urls import urlpatterns as openid_urlpatterns
+
 urlpatterns = patterns(
-    'localtv.views',
+    'localtv.views.subsite',
     (r'^$', 'subsite_index', {}, 'localtv_subsite_index'),
     (r'^video/(?P<video_id>[0-9]+)/$', 'view_video',
      {}, 'localtv_subsite_view_video'))
+
+urlpatterns += openid_urlpatterns
