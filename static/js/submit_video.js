39c3f5bd1f67d203b7cab63a85c1904392411277
Christopher Allan Webber <cwebber@dustycloud.org>
2009-03-16 00:35:21 -0500

    Horray!  A shitty box with which we can do video ajaxy submission

--- static/js/submit_video.js
+++ static/js/submit_video.js
@@ -1,1 +1,14 @@
-alert("Hi everybody!");
+function start_submit_process() {
+    $('#hover_wrap').css('display', 'block');
+    return false;
+}
+
+function close_submit_box() {
+    $('#hover_wrap').css('display', 'none');
+    return false;
+}    
+
+function add_submit_callback() {
+    $('#submit_button').click(start_submit_process);
+    $('#close_submit_box span').click(close_submit_box);
+}
