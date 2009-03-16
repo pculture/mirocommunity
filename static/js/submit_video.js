db7ec99c1eb3f3316543bf26112bcd8b71e91060
Christopher Allan Webber <cwebber@dustycloud.org>
2009-03-16 01:13:05 -0500

    Awsnap, ajax video submission *works*

--- static/js/submit_video.js
+++ static/js/submit_video.js
@@ -1,4 +1,12 @@
+// maybe not the most elegantly named function.
+function replace_submit_video_and_setup_callbacks(result) {
+    $('#submit_box_content').html(result);
+    $('#submit_box_content form').ajaxForm(replace_submit_video_and_setup_callbacks);
+}
+
 function start_submit_process() {
+    jQuery.get($('#submit_button').attr('href'),
+               replace_submit_video_and_setup_callbacks);
     $('#hover_wrap').css('display', 'block');
     return false;
 }
