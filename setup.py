6e2db8d1de3135706be1c7d18b3df09d23e2865e
Christopher Allan Webber <cwebber@dustycloud.org>
2009-03-20 16:57:52 -0500

    Added a commandline tool to do the feed updating from

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(name="Miro Local TV",
+      version=version,
+      author='Participatory Culture Foundation',
+      license='AGPLv3',
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      localtv_update_feeds = localtv.feedimport:update_feeds
+      """)
+      
+
+
