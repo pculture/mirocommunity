d4c311ff1f464e645ee67e5490766c32eaa4f2cd
Christopher Allan Webber <cwebber@dustycloud.org>
2009-03-25 23:28:20 -0500

    Listing kinda works on the frontpage

--- localtv/subsite/views.py
+++ localtv/subsite/views.py
@@ -7,10 +7,15 @@ from localtv.decorators import get_sitelocation
 
 @get_sitelocation
 def subsite_index(request, sitelocation=None):
+    featured_videos = models.Video.objects.filter(
+        site=sitelocation.site, last_featured__isnull=False)
+    featured_videos = featured_videos.order_by(
+        '-last_featured', '-when_submitted')
     return render_to_response(
         'localtv/subsite/index.html',
         {'sitelocation': sitelocation,
-         'request': request},
+         'request': request,
+         'featured_videos': featured_videos},
         context_instance=RequestContext(request))
 
 
