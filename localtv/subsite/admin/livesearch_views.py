7c09cc3127a5056a6631caa4846228c7027ef000
Christopher Allan Webber <cwebber@dustycloud.org>
2009-04-20 10:02:31 -0500

    Can now click-to-view videos with livesearch, which works pretty good.

--- localtv/subsite/admin/livesearch_views.py
+++ localtv/subsite/admin/livesearch_views.py
@@ -8,6 +8,43 @@ from localtv.decorators import get_sitelocation
 from localtv import util
 
 
+## ----------
+## Decorators
+## ----------
+
+def get_search_video(view_func):
+    def new_view_func(request, *args, **kwargs):
+        query_string = request.GET.get('query')
+        session_searches = request.session.get('localtv_livesearches')
+
+        if not session_searches or not session_searches.get(query_string):
+            return HttpResponseBadRequest(
+                'No matching livesearch results in your session')
+
+        search_video = None
+        for this_result in session_searches.get(query_string):
+            if this_result.id == int(request.GET['video_id']):
+                search_video = this_result
+                break
+
+        if not search_video:
+            return HttpResponseBadRequest(
+                'No specific video matches that id in this queryset')
+
+        return view_func(request, search_video=search_video, *args, **kwargs)
+
+    # make decorator safe
+    new_view_func.__name__ = view_func.__name__
+    new_view_func.__dict__ = view_func.__dict__
+    new_view_func.__doc__ = view_func.__doc__
+
+    return new_view_func
+
+
+## ----------
+## Views
+## ----------
+
 @get_sitelocation
 def livesearch_page(request, sitelocation=None):
     query_string = request.GET.get('query')
@@ -40,25 +77,17 @@ def livesearch_page(request, sitelocation=None):
 
 
 @get_sitelocation
-def approve(request, sitelocation=None):
-    query_string = request.GET.get('query')
-    session_searches = request.session.get('localtv_livesearches')
-
-    if not session_searches or not session_searches.get(query_string):
-        return HttpResponseBadRequest(
-            'No matching livesearch results in your session')
-
-    search_video = None
-    for this_result in session_searches.get(query_string):
-        if this_result.id == int(request.GET['video_id']):
-            search_video = this_result
-            break
-
+@get_search_video
+def approve(request, search_video, sitelocation=None):
     search_video.generate_video_model(sitelocation.site)
     
     return HttpResponse('SUCCESS')
 
 
 @get_sitelocation
-def display(request, sitelocation=None):
-    pass
+@get_search_video
+def display(request, search_video, sitelocation=None):
+    return render_to_response(
+        'localtv/subsite/admin/video_preview.html',
+        {'current_video': search_video},
+        context_instance=RequestContext(request))
