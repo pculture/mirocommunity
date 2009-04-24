72611fc2c5ee1ef7b028faea7b4b128497e7a4f8
Christopher Allan Webber <cwebber@dustycloud.org>
2009-04-23 19:28:05 -0500

    We don't need openid/ in these urls any more

--- localtv/openid/urls.py
+++ localtv/openid/urls.py
@@ -4,13 +4,13 @@ from localtv.openid.views import redirect_to_login_or_register
 
 urlpatterns = patterns(
     '',
-    (r'^openid/$', 'django_openidconsumer.views.begin',
+    (r'^$', 'django_openidconsumer.views.begin',
      {'sreg': 'email,nickname'}, 'localtv_openid_start'),
-    (r'^openid/complete/$', 'django_openidconsumer.views.complete',
+    (r'^complete/$', 'django_openidconsumer.views.complete',
      {'on_success': redirect_to_login_or_register}, 'localtv_openid_complete'),
-    (r'^openid/login_or_register/$', 'localtv.openid.views.login_or_register',
+    (r'^login_or_register/$', 'localtv.openid.views.login_or_register',
      {}, 'localtv_openid_login_or_register'),
-    (r'^openid/signout/$', 'localtv.openid.views.signout',
+    (r'^signout/$', 'localtv.openid.views.signout',
      {}, 'localtv_openid_signout'),
 )
 
