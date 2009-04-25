08fb93c8750b09cbe072b2cb5270058df1c99c13
Christopher Allan Webber <cwebber@dustycloud.org>
2009-04-24 19:02:54 -0500

    ah yeah, Need the evolutions __init__ too

--- localtv/evolutions/__init__.py
+++ localtv/evolutions/__init__.py
@@ -1,9 +1,1 @@
-SEQUENCE = [
-    'add_openid_name_and_email',
-    'unrequire_feed',
-    'add_embed_code',
-    'add_guid',
-    'non_unique_feeds',
-    'featured_videos',
-    'add_auto_approve_field',
-]
+SEQUENCE = []
