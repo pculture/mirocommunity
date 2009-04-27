cbe7ea3c7c2e82b00bce94208c4f0d56ad2713ca
Christopher Allan Webber <cwebber@dustycloud.org>
2009-04-26 21:19:40 -0500

    Adding basic search.  Also correcting the queries for frontpage feed listings

--- localtv/subsite/urls.py
+++ localtv/subsite/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include
 urlpatterns = patterns(
     'localtv.subsite.views',
     (r'^$', 'subsite_index', {}, 'localtv_subsite_index'),
+    (r'^search/$', 'video_search', {}, 'localtv_subsite_search'),
     (r'^video/(?P<video_id>[0-9]+)/$', 'view_video',
      {}, 'localtv_view_video'))
 
--- localtv/subsite/views.py
+++ localtv/subsite/views.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.db.models import Q
+from django.views.generic.list_detail import object_list
 
 from localtv import models
 from localtv.decorators import get_sitelocation
@@ -9,12 +11,16 @@ from localtv.decorators import get_sitelocation
 @get_sitelocation
 def subsite_index(request, sitelocation=None):
     featured_videos = models.Video.objects.filter(
-        site=sitelocation.site, last_featured__isnull=False)
+        site=sitelocation.site,
+        status=models.VIDEO_STATUS_ACTIVE,
+        last_featured__isnull=False)
     featured_videos = featured_videos.order_by(
         '-last_featured', '-when_submitted')[:10]
-    new_videos = models.Video.objects.filter(site=sitelocation.site)
+    new_videos = models.Video.objects.filter(
+        site=sitelocation.site,
+        status=models.VIDEO_STATUS_ACTIVE)
     new_videos = new_videos.order_by(
-        '-last_featured', '-when_submitted')[:10]
+        '-when_submitted')[:10]
 
     return render_to_response(
         'localtv/subsite/index.html',
@@ -35,3 +41,39 @@ def view_video(request, video_id, sitelocation=None):
          'current_video': video,
          'intensedebate_acct': getattr(settings, 'LOCALTV_INTENSEDEBATE_ACCT')},
         context_instance=RequestContext(request))
+
+
+@get_sitelocation
+def video_search(request, sitelocation=None):
+    query_string = request.GET.get('query', '')
+
+    if query_string:
+        terms = set(query_string.split())
+
+        exclude_terms = set([
+                component for component in terms if component.startswith('-')])
+        include_terms = terms.difference(exclude_terms)
+        stripped_exclude_terms = [term.lstrip('-') for term in exclude_terms]
+
+        videos = models.Video.objects.filter(
+            site=sitelocation.site,
+            status=models.VIDEO_STATUS_ACTIVE)
+
+        for term in include_terms:
+            videos = videos.filter(
+                Q(description__icontains=term) | Q(name__icontains=term))
+
+        for term in stripped_exclude_terms:
+            videos = videos.exclude(
+                Q(description__icontains=term) | Q(name__icontains=term))
+
+        return object_list(
+            request=request, queryset=videos,
+            paginate_by=15,
+            template_name='localtv/subsite/video_listing.html',
+            allow_empty=True, template_object_name='video')
+
+    else:
+        pass
+
+        
