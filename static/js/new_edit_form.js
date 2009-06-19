77efb8aa96154b13b1880e38e00c6c90e2a1acfd
Christopher Allan Webber <cwebber@dustycloud.org>
2009-06-19 10:56:06 -0500

    clicking on this now toggles the display of that widget

--- static/js/new_edit_form.js
+++ static/js/new_edit_form.js
@@ -1,17 +1,34 @@
 function inline_edit_open() {
-    obj = $(this);
-    editable_wrapper = obj.parent('.editable');
-    
+    var obj = $(this);
+    var editable_wrapper = obj.parent('.editable');
+    obj.css('display', 'none');
+    editable_wrapper.children('.input_field').css('display', 'inline');
 }
 
 function insert_and_activate_action_buttons() {
-    obj = $(this);
+    var obj = $(this);
     obj.append('<span class="save">✔</span> <span class="cancel">✖</span>');
     obj.children('.save').click(inline_save);
     obj.children('.cancel').click(inline_cancel);
 }
 
+function inline_save() {
+    alert('saved, mothafuckas!');
+}
+
+function inline_cancel() {
+    var obj = $(this);
+    var editable_wrapper = obj.parent().parent('.editable');
+    obj.parent().css('display', 'none');
+    editable_wrapper.children('.display_data').css('display', 'inline');
+}
+
 function edit_widgets_setup() {
     $(".editable .display_data").click(inline_edit_open);
-    $(".editable .input_field").click(insert_and_activate_action_buttons);
+    var editable_inputs = $(".editable .input_field");
+    editable_inputs.append('<span class="save">✔</span> <span class="cancel">✖</span>');
+    editable_inputs.children('.save').click(inline_save);
+    editable_inputs.children('.cancel').click(inline_cancel);
 }
+
+$(document).ready(edit_widgets_setup);
