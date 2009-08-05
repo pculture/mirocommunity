d9e7aa59a2c022614beb03fcc90a86c848840d81
Paul Swartz <pswartz@pculture.org>
2009-08-05 12:31:26 -0400

    Jesse's mainsite

--- localtv/mainsite/urls.py
+++ localtv/mainsite/urls.py
@@ -5,4 +5,9 @@ urlpatterns = patterns(
     (r'^$',
      'django.views.generic.simple.direct_to_template',
      {'template': 'localtv/mainsite/index.html'},
-     'localtv_mainsite_index'))
+     'localtv_mainsite_index'),
+    (r'^college/?$',
+     'django.views.generic.simple.direct_to_template',
+     {'template': 'localtv/mainsite/college.html'},
+     'localtv_mainsite_index'),
+    )
