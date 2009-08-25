be7326e3696fcaec8cdabb971194452cbdfc0384
Paul Swartz <pswartz@pculture.org>
2009-08-25 16:52:14 -0400

    add a script to update missing publish dates

--- localtv/feedimport.py
+++ localtv/feedimport.py
@@ -7,3 +7,16 @@ def update_feeds(verbose=False):
 def update_saved_searches(verbose=False):
     for saved_search in models.SavedSearch.objects.all():
         saved_search.update_items()
+
+
+def update_publish_date(verbose=False):
+    import vidscraper
+    for v in models.Video.objects.filter(when_published__isnull=True):
+        try:
+            d = vidscraper.auto_scrape(v.website_url, fields=['publish_date'])
+        except:
+            pass
+        else:
+            if d:
+                v.when_published = d['publish_date']
+                v.save()
--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name="Miro Community",
       [console_scripts]
       localtv_update_feeds = localtv.feedimport:update_feeds
       localtv_update_saved_searches = localtv.feedimport:update_saved_searches
+      localtv_update_publish_date = localtv.feedimport:update_publish_date
       """)
       
 
