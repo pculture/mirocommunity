6045cffa3184d9f99664d62ae23b98a62f418a34
Paul Swartz <pswartz@pculture.org>
2009-08-31 15:50:57 -0400

    add a UserCreationForm to the users page

--- localtv/subsite/admin/user_views.py
+++ localtv/subsite/admin/user_views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -10,6 +11,8 @@ from localtv.subsite.admin import forms
 @get_sitelocation
 def users(request, sitelocation=None):
     admins = sitelocation.admins_user.all()
+    add_user_form = forms.AddUserForm()
+    create_user_form = UserCreationForm()
     if request.method == 'POST':
         if request.POST['submit'] == 'Add':
             add_user_form = forms.AddUserForm(request.POST)
@@ -17,11 +20,12 @@ def users(request, sitelocation=None):
                 users = add_user_form.cleaned_data['user']
                 sitelocation.admins_user.add(*users)
                 return HttpResponseRedirect(request.path)
-
-            return render_to_response('localtv/subsite/admin/users.html',
-                                      {'admins': admins,
-                                       'add_user_form': add_user_form},
-                                      context_instance=RequestContext(request))
+        elif request.POST['submit'] == 'Create':
+            create_user_form = UserCreationForm(request.POST)
+            if create_user_form.is_valid():
+                user = create_user_form.save()
+                sitelocation.admins_user.add(user)
+                return HttpResponseRedirect(request.path)
         elif request.POST['submit'] == 'Delete':
             user_id = request.POST.get('id')
             if user_id is not None:
@@ -34,9 +38,9 @@ def users(request, sitelocation=None):
             return HttpResponseRedirect(request.path)
         else:
             return HttpResponseRedirect(request.path)
-    else:
-        add_user_form = forms.AddUserForm()
-        return render_to_response('localtv/subsite/admin/users.html',
-                                  {'admins': admins,
-                                   'add_user_form': add_user_form},
-                                  context_instance=RequestContext(request))
+
+    return render_to_response('localtv/subsite/admin/users.html',
+                              {'admins': admins,
+                               'add_user_form': add_user_form,
+                               'create_user_form': create_user_form},
+                              context_instance=RequestContext(request))
