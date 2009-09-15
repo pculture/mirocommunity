4bace68ee9ba8083f890868a85eb74656832d9d1
Paul Swartz <pswartz@pculture.org>
2009-09-15 16:37:09 -0400

    Revert "Adding license headers."
    For templates that inherit from another one, the {% extends %} tag has to come
    first; I'll come back and fix this later.
    
    This reverts commit 57dd4ef26ebc41489a7abf00b2654016d6805994.

--- localtv/subsite/admin/manage_views.py
+++ localtv/subsite/admin/manage_views.py
@@ -1,17 +1,3 @@
-# Miro Community
-# Copyright 2009 - Participatory Culture Foundation
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-# 
-# You should have received a copy of the GNU Affero General Public License
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
