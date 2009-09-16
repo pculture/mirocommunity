57b3998ffe69a1476c18e57ecb50c721ac3f1040
Paul Swartz <pswartz@pculture.org>
2009-09-16 14:20:35 -0400

    fix editing on the manage sources page

--- localtv/subsite/admin/edit_attributes/urls.py
+++ localtv/subsite/admin/edit_attributes/urls.py
@@ -5,7 +5,7 @@ from localtv import models
 urlpatterns = patterns(
     'localtv.subsite.admin.edit_attributes',
     (r'^feed/(?P<id>[0-9]+)/name/$', 'simple.edit_field',
-     {'model': models.Feed, 'field': 'name'}, 'localtv_admin_feed_edit_title'),
+     {'model': models.Feed, 'field': 'name'}, 'localtv_admin_feed_edit_name'),
     (r'^feed/(?P<id>[0-9]+)/auto_categories/$',
      'feed_views.edit_auto_categories',
      {}, 'localtv_admin_feed_edit_auto_categories'),
