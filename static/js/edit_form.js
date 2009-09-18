b70a0212b962ec54309aa00f99e5b9040320e0fd
Paul Swartz <pswartz@pculture.org>
2009-09-18 15:37:47 -0400

    reenable uploading a thumbnail

--- static/js/edit_form.js
+++ static/js/edit_form.js
@@ -1,8 +1,5 @@
 function inline_edit_open() {
     obj = $(this);
-    if (obj.parent().hasClass('description')) {
-        obj = obj.parent();
-    }
     if (obj.hasClass('open')) {
         is_file = Boolean(obj.children('input[type=file]').length);
         return is_file; // file inputs need return true
@@ -10,31 +7,13 @@ function inline_edit_open() {
     obj.addClass('open');
     obj[0].oldContent = obj.html();
     obj.text('');
-    if (obj.hasClass('vid_title')) {
-        input = $('<input type="text" />').val($("#id_name").val());
-        obj.append(input);
-    } else if (obj.hasClass('description')) {
-        textarea = $('<textarea cols="40" rows="10"/>').val($("#id_description").val());
-        obj.append(textarea);
-    } else if (obj.hasClass('thumbnail')) {
+    if (obj.hasClass('thumbnail')) {
         input = $('<input type="text" />').val($("#id_thumbnail_url").val());
         input2 = $('<input type="file" name="thumbnail"/>');
         obj.append('Thumbnail URL');
         obj.append(input);
         obj.append('or upload: ');
         obj.append(input2);
-    } else if (obj.hasClass('tags')) {
-        input = $('<input type="text" />').val($("#id_tags").val());
-        obj.append(input);
-    } else if (obj.hasClass('categories')) {
-        input = $("#id_categories").clone().attr('id', '');
-        obj.append(input);
-    } else if (obj.hasClass('authors')) {
-        input = $("#id_authors").clone().attr('id', '');
-        obj.append(input);
-    } else if (obj.hasClass('date')) {
-        input = $('<input type="text" />').val($("#id_when_published").val());
-        obj.append(input);
     } else {
         obj.removeClass('open');
         obj.html(this.oldContent);
@@ -48,15 +27,7 @@ function inline_edit_open() {
 
 function inline_save() {
     obj = $(this).parent();
-    if (obj.hasClass('vid_title')) {
-        value = obj.children('input').val();
-        $("#id_name").val(value);
-        inline_post(obj);
-    } else if (obj.hasClass('description')) {
-        value = obj.children('textarea').val();
-        $('#id_description').val(value);
-        inline_post(obj);
-    } else if (obj.hasClass('thumbnail')) {
+    if (obj.hasClass('thumbnail')) {
         input = obj.children('input');
         input.replaceWith($('<span>Uploading...</span>'));
         $("#id_thumbnail_url").val(input.eq(0).val());
@@ -64,23 +35,7 @@ function inline_save() {
         old_input.after(input.eq(1));
         old_input.remove();
         inline_post(obj);
-    } else if (obj.hasClass('tags')) {
-        value = obj.children('input').val();
-        $("#id_tags").val(value);
-        inline_post(obj);
-    } else if (obj.hasClass('categories')) {
-        value = obj.children('select').val();
-        $("#id_categories").val(value);
-        inline_post(obj);
-    } else if (obj.hasClass('authors')) {
-        value = obj.children('select').val();
-        $("#id_authors").val(value);
-        inline_post(obj);
-    } else if (obj.hasClass('date')) {
-        value = obj.children('input').val();
-        $("#id_when_published").val(value);
-        inline_post();
-    }
+     }
 }
 
 function inline_cancel() {
