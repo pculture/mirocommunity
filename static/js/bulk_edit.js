f734fb16c4e6101bf75d1fae243c94f13e3e67fc
Paul Swartz <pswartz@pculture.org>
2009-09-23 18:15:50 -0400

    get the delete button to submit correctly on the users page
    I think there's a bad interaction between the named submit buttons and the
    submit() method, so we just click on one of the submit buttons instead.

--- static/js/bulk_edit.js
+++ static/js/bulk_edit.js
@@ -19,7 +19,7 @@ function showEdit(obj, show) {
 function toggleDelete(obj) {
     obj = $(obj);
     obj.next().val('checked');
-    $("#labels form:eq(1)").submit();
+    $("#labels form:last input[type=submit]:eq(0)").click();
     return false;
 }
 
