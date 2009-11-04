4634c09267108f71e7716f6ecd9af175a5b1c2ce
Paul Swartz <paulswartz@gmail.com>
2009-11-04 16:10:43 -0500

    80 characters

--- localtv/models.py
+++ localtv/models.py
@@ -685,8 +685,10 @@ class Video(models.Model):
     thumbnail_extension = models.CharField(max_length=8, blank=True)
     user = models.ForeignKey('auth.User', null=True, blank=True)
     search = models.ForeignKey(SavedSearch, null=True, blank=True)
-    video_service_user = models.CharField(max_length=250, blank=True, default='')
-    video_service_url = models.URLField(verify_exists=False, blank=True, default='')
+    video_service_user = models.CharField(max_length=250, blank=True,
+                                          default='')
+    video_service_url = models.URLField(verify_exists=False, blank=True,
+                                        default='')
 
     objects = VideoManager()
 
