bddf246b52c78ffd350cde5c0c8a109793fdc601
Paul Swartz <paulswartz@gmail.com>
2010-01-26 10:17:41 -0500

    Bug #12762: suspend old instances of MC to free up resources
    If no user has logged in for 2 months, don't bother scanning feeds or searches.

--- localtv/feedimport.py
+++ localtv/feedimport.py
@@ -15,20 +15,40 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Miro Community.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
+
 from django.core.files.storage import default_storage
+from django.contrib.auth.models import User
 
 from localtv import models
+
+TWO_MONTHS = datetime.timedelta(days=62)
+
+def site_too_old():
+    if User.objects.order_by('-last_login').values_list(
+        'last_login', flat=True)[0] + TWO_MONTHS < datetime.now():
+        return True
+    else:
+        return False
+
+
 def update_feeds(verbose=False):
+    if site_too_old():
+        return
     for feed in models.Feed.objects.filter(status=models.FEED_STATUS_ACTIVE):
         feed.update_items()
 
 
 def update_saved_searches(verbose=False):
+    if site_too_old():
+        return
     for saved_search in models.SavedSearch.objects.all():
         saved_search.update_items()
 
 
 def update_publish_date(verbose=False):
+    if site_too_old():
+        return
     import vidscraper
     for v in models.Video.objects.filter(when_published__isnull=True):
         try:
@@ -41,6 +61,8 @@ def update_publish_date(verbose=False):
                 v.save()
 
 def update_thumbnails(verbose=False):
+    if site_too_old():
+        return
     for v in models.Video.objects.filter(has_thumbnail=True):
         path = v.get_original_thumb_storage_path()
         if not default_storage.exists(path):
@@ -50,4 +72,4 @@ def update_thumbnails(verbose=False):
             except Exception:
                 import traceback
                 traceback.print_exc()
-                
+
