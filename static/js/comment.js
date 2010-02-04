1e0f7309355be1513e3137ed255807bd08b3e105
Paul Swartz <paulswartz@gmail.com>
2010-02-04 10:47:54 -0500

    Bug #12281: When Commenting, stay on View Video Page

---
+++ static/js/comment.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("h2.post-comment-title + form").ajaxForm({success: function (response) {
+        response = $(response);
+        if (response.children('#next').length) {
+            location.href = response.children('#next').attr('href');
+        } else {
+            $("#hover_wrap").find('.contentWrap').html(response);
+            wrap = $("#hover_wrap").overlay({api: true});
+            console.log(wrap.getContent());
+            wrap.getContent().find('form:eq(0)').ajaxForm(
+                replace_submit_video_and_setup_callbacks);
+            wrap.load();
+        }
+    }});
+});
