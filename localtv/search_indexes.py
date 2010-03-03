f14b02e8f7b5785eaab5cf4aaae784ba71b2b694
Paul Swartz <paulswartz@gmail.com>
2010-03-03 15:54:40 -0500

    don't actually need the best_date in the index anymore

--- localtv/search_indexes.py
+++ localtv/search_indexes.py
@@ -5,12 +5,11 @@ from localtv.models import Video, VIDEO_STATUS_ACTIVE
 
 class VideoIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
-    best_date = indexes.DateTimeField(model_attr='best_date')
 
     def get_queryset(self):
         """
         Custom queryset to only search approved videos.
         """
-        return Video.objects.new(status=VIDEO_STATUS_ACTIVE)
+        return Video.objects.filter(status=VIDEO_STATUS_ACTIVE)
 
 site.register(Video, VideoIndex)
