20af708c7e4ea466eee4fbebf74cc079854a7660
Paul Swartz <paulswartz@gmail.com>
2010-03-03 15:53:48 -0500

    Bug #12899, #12922: allow sorting by data on featured, search views
    if ?sort=latest is added to the featured or search views, the results will be
    ordered by the newness of the videos, rather than the default ordering.

--- localtv/listing/views.py
+++ localtv/listing/views.py
@@ -16,23 +16,22 @@
 # along with Miro Community.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
+import operator
 
 from django.contrib.auth.models import User
-from django.core.paginator import Paginator, InvalidPage
 from django.db.models import Q
-from django.http import Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.generic.list_detail import object_list
 
 from tagging.models import Tag
 
-import haystack.forms
+import haystack.forms, haystack.query
 
-from localtv import models
+from localtv import models, util
 from localtv.decorators import get_sitelocation
 
-def get_count(func):
+def get_args(func):
     def wrapper(request, *args, **kwargs):
         count = request.GET.get('count')
         if count:
@@ -40,8 +39,13 @@ def get_count(func):
                 count = int(count)
             except ValueError:
                 count = None
-        if count is not None:
+        if count:
             kwargs['count'] = count
+        sort = request.GET.get('sort')
+        if sort not in ('latest',):
+            sort = None
+        if sort:
+            kwargs['sort'] = sort
         return func(request, *args, **kwargs)
     return wrapper
 
@@ -51,9 +55,9 @@ def index(request, sitelocation=None):
         'localtv/browse.html', {},
         context_instance=RequestContext(request))
 
-@get_count
+@get_args
 @get_sitelocation
-def new_videos(request, sitelocation=None, count=15):
+def new_videos(request, sitelocation=None, count=15, sort=None):
     videos = models.Video.objects.new(
         site=sitelocation.site,
         status=models.VIDEO_STATUS_ACTIVE)
@@ -63,9 +67,9 @@ def new_videos(request, sitelocation=None, count=15):
         template_name='localtv/video_listing_new.html',
         allow_empty=True, template_object_name='video')
 
-@get_count
+@get_args
 @get_sitelocation
-def popular_videos(request, sitelocation=None, count=15):
+def popular_videos(request, sitelocation=None, count=15, sort=None):
     period = datetime.timedelta(days=7)
     videos = models.Video.objects.popular_since(
         period, sitelocation,
@@ -78,24 +82,29 @@ def popular_videos(request, sitelocation=None, count=15):
         template_name='localtv/video_listing_popular.html',
         allow_empty=True, template_object_name='video')
 
-@get_count
+@get_args
 @get_sitelocation
-def featured_videos(request, sitelocation=None, count=15):
-    videos = models.Video.objects.filter(
-        site=sitelocation.site, last_featured__isnull=False,
-        status=models.VIDEO_STATUS_ACTIVE)
-    videos = videos.order_by(
-        '-last_featured', '-when_approved', '-when_published',
-        '-when_submitted')
+def featured_videos(request, sitelocation=None, count=15, sort=None):
+    kwargs = {
+        'site': sitelocation.site,
+        'last_featured__isnull': False,
+        'status': models.VIDEO_STATUS_ACTIVE}
+    if sort == 'latest':
+        videos = models.Video.objects.new(**kwargs)
+    else:
+        videos = models.Video.objects.filter(**kwargs)
+        videos = videos.order_by(
+            '-last_featured', '-when_approved', '-when_published',
+            '-when_submitted')
     return object_list(
         request=request, queryset=videos,
         paginate_by=count,
         template_name='localtv/video_listing_featured.html',
         allow_empty=True, template_object_name='video')
 
-@get_count
+@get_args
 @get_sitelocation
-def tag_videos(request, tag_name, sitelocation=None, count=15):
+def tag_videos(request, tag_name, sitelocation=None, count=15, sort=None):
     tag = get_object_or_404(Tag, name=tag_name)
     videos = models.Video.tagged.with_all(tag).filter(
         site=sitelocation.site,
@@ -109,9 +118,9 @@ def tag_videos(request, tag_name, sitelocation=None, count=15):
         allow_empty=True, template_object_name='video',
         extra_context={'tag': tag})
 
-@get_count
+@get_args
 @get_sitelocation
-def feed_videos(request, feed_id, sitelocation=None, count=15):
+def feed_videos(request, feed_id, sitelocation=None, count=15, sort=None):
     feed = get_object_or_404(models.Feed, pk=feed_id,
                              site=sitelocation.site)
     videos = models.Video.objects.new(site=sitelocation.site,
@@ -125,11 +134,11 @@ def feed_videos(request, feed_id, sitelocation=None, count=15):
         extra_context={'feed': feed})
 
 
-@get_count
+@get_args
 @get_sitelocation
-def video_search(request, sitelocation=None, count=10):
+def video_search(request, sitelocation=None, count=10, sort=None):
     query = ''
-    results = []
+    pks = []
 
     if 'query' in request.GET and 'q' not in request.GET:
         # old-style templates
@@ -138,35 +147,40 @@ def video_search(request, sitelocation=None, count=10):
         request.GET = GET
 
     if request.GET.get('q'):
-        form = haystack.forms.ModelSearchForm(request.GET, load_all=True)
+        form = haystack.forms.ModelSearchForm(
+            request.GET,
+            searchqueryset=haystack.query.RelatedSearchQuerySet())
 
         if form.is_valid():
             query = form.cleaned_data['q']
             results = form.search()
+            pks = [result.pk for result in results if result is not None]
     else:
         form = haystack.forms.ModelSearchForm()
+    if not pks:
+        queryset = models.Video.objects.empty()
+    elif sort == 'latest':
+        queryset = models.Video.objects.new(
+            site=sitelocation.site,
+            status=models.VIDEO_STATUS_ACTIVE,
+            pk__in=pks)
+    else:
+        queryset = models.Video.objects.filter(
+                site=sitelocation.site,
+                status=models.VIDEO_STATUS_ACTIVE,
+                pk__in=pks).order_by()
+        order = ['-localtv_video.id = %i' % pk for pk in pks]
+        queryset = queryset.extra(order_by=order)
+    return object_list(
+        request=request, queryset=queryset,
+        paginate_by=count,
+        template_name='localtv/video_listing_search.html',
+        allow_empty=True, template_object_name='video',
+        extra_context={'query': query})
 
-    paginator = Paginator(results, count)
-
-    try:
-        page = paginator.page(int(request.GET.get('page', 1)))
-    except InvalidPage:
-        raise Http404("No such page of results!")
-
-    context = { # mimic the object_list context
-        'form': form,
-        'page_obj': page,
-        'video_list': [result.object for result in page.object_list if result],
-        'paginator': paginator,
-        'query': query,
-    }
-
-    return render_to_response('localtv/video_listing_search.html', context,
-                              context_instance=RequestContext(request))
-
-@get_count
+@get_args
 @get_sitelocation
-def category(request, slug=None, sitelocation=None, count=15):
+def category(request, slug=None, sitelocation=None, count=15, sort=None):
     if slug is None:
         categories = models.Category.objects.filter(
             site=sitelocation.site,
@@ -174,7 +188,7 @@ def category(request, slug=None, sitelocation=None, count=15):
 
         return object_list(
             request=request, queryset=categories,
-            paginate_by=18,
+            paginate_by=count,
             template_name='localtv/categories.html',
             allow_empty=True, template_object_name='category')
     else:
@@ -187,9 +201,9 @@ def category(request, slug=None, sitelocation=None, count=15):
             allow_empty=True, template_object_name='video',
             extra_context={'category': category})
 
-@get_count
+@get_args
 @get_sitelocation
-def author(request, id=None, sitelocation=None, count=15):
+def author(request, id=None, sitelocation=None, count=15, sort=None):
     if id is None:
         return render_to_response(
             'localtv/author_list.html',
@@ -198,7 +212,11 @@ def author(request, id=None, sitelocation=None, count=15):
     else:
         author = get_object_or_404(User,
                                    pk=id)
-        videos = models.Video.objects.filter(
+        if sort:
+            videos = models.Video.objects.new()
+        else:
+            videos = models.Video.objects.all()
+        videos = videos.filter(
             Q(authors=author) | Q(user=author),
             site=sitelocation.site,
             status=models.VIDEO_STATUS_ACTIVE).distinct()
