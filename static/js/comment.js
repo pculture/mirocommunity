16d5d5d254b52bc397b21475a1e6ff4da3f4c111
Paul Swartz <paulswartz@gmail.com>
2010-04-07 15:30:55 -0400

    Bug #12281: When Commenting, stay on View Video Page

---
+++ static/js/comment.js
@@ -0,0 +1,31 @@
+function comment_form_callback(response) {
+    parsed = $("<div/>").html(response);
+    if (parsed.find('#next').attr('href') == location.pathname) {
+        location.href = location.href + '#comments';
+        location.reload()
+        return;
+    }
+    form = parsed.find('form')
+    if (form.length) {
+        // problems with the form submission
+        $("#comment_form").replaceWith(form);
+        recaptcha_ajax_field = $("#comment_form #recaptcha_ajax_field");
+        if (typeof recaptcha_ajax_callback !== 'undefined') {
+            recaptcha_ajax_callback();
+        }
+        $("#comment_form").ajaxForm(comment_form_ajax_options);
+    } else {
+        $("#comment_form").clearForm().find('.errorlist').remove();
+        $("#overlay .contentWrap").empty().append(parsed.find('.comment_posted'));
+        $("#overlay").overlay({api:true}).load();
+    }
+}
+
+comment_form_ajax_options = {
+    success: comment_form_callback,
+    dataType: 'html'
+}
+
+$(document).ready(function() {
+    $("#comment_form").ajaxForm(comment_form_ajax_options);
+});
