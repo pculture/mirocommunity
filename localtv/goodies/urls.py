fd2c59c2a42d7a879f3e9244a9b1676e6c598418
Paul Swartz <paulswartz@gmail.com>
2010-05-10 11:13:36 -0400

    changes to the backend from Anne

--- localtv/goodies/urls.py
+++ localtv/goodies/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls.defaults import patterns
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    (r'^$', direct_to_template, {
-            'template': 'localtv/goodies/index.html'},
-     'localtv_goodies'),
     (r'^widget/$', direct_to_template, {
             'template': 'localtv/goodies/widget.html'},
      'localtv_goodies_widget'),
