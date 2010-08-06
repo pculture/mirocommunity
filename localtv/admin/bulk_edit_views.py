e209041bcbd5071ffe621db484635b469557ecf3
Paul Swartz <z3p@ender.(none)>
2010-08-06 09:19:50 -0400

    show fewer items on the bulk edit page

--- localtv/admin/bulk_edit_views.py
+++ localtv/admin/bulk_edit_views.py
@@ -114,7 +114,7 @@ def bulk_edit(request, sitelocation=None):
             sort.replace('name', 'name_lower'))
     else:
         videos = videos.order_by(sort)
-    video_paginator = Paginator(videos, 50)
+    video_paginator = Paginator(videos, 30)
     try:
         page = video_paginator.page(int(request.GET.get('page', 1)))
     except ValueError:
