93ffa9947773e6595da83f50cb89a58855dda06c
Paul Swartz <paulswartz@gmail.com>
2010-08-18 12:16:05 -0400

    Bug #14139: Additional search keywords
    playlist:#
    playlist:user/slug
    category:#
    category:name
    category:slug
    user:#
    user:username
    feed:#
    feed:name
    search:#
    search:query string
    tag:name
    {one of these terms}
    -"not this term"

--- localtv/search_indexes.py
+++ localtv/search_indexes.py
@@ -5,6 +5,13 @@ from localtv.models import Video, VIDEO_STATUS_ACTIVE
 
 class VideoIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
+    feed = indexes.IntegerField(model_attr='feed__pk', null=True)
+    search = indexes.IntegerField(model_attr='search__pk', null=True)
+    user = indexes.IntegerField(model_attr='user__pk', null=True)
+    tags = indexes.MultiValueField()
+    categories = indexes.MultiValueField()
+    authors = indexes.MultiValueField()
+    playlists = indexes.MultiValueField()
 
     def get_queryset(self):
         """
@@ -14,5 +21,21 @@ class VideoIndex(indexes.SearchIndex):
 
     def get_updated_field(self):
         return 'when_modified'
+
+    def _prepare_field(self, video, field, attr='pk', normalize=int):
+        return [normalize(getattr(rel, attr))
+                for rel in getattr(video, field).all()]
+
+    def prepare_tags(self, video):
+        return self._prepare_field(video, 'tags', 'name', unicode)
+
+    def prepare_categories(self, video):
+        return self._prepare_field(video, 'categories')
+
+    def prepare_authors(self, video):
+        return self._prepare_field(video, 'authors')
+
+    def prepare_playlists(self, video):
+        return self._prepare_field(video, 'playlists')
 
 site.register(Video, VideoIndex)
