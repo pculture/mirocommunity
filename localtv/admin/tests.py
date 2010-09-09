c94d10ef1f1a8e32dc266368eef6879fd10d30e6
Paul Swartz <paulswartz@gmail.com>
2010-09-09 14:51:01 -0400

    Bug #14498: don't re-shorten the name of the logo/background files

--- localtv/admin/tests.py
+++ localtv/admin/tests.py
@@ -2891,6 +2891,25 @@ class EditSettingsAdministrationTestCase(AdministrationBaseTestCase):
         site_location.background.open()
         self.assertEquals(site_location.background.read(), logo_data)
 
+        logo_name = site_location.logo.name
+        background_name = site_location.background.name
+        # don't send them again, and make sure the names stay the same
+        del self.POST_data['logo']
+        del self.POST_data['background']
+
+        POST_response = c.post(self.url, self.POST_data)
+        self.assertStatusCodeEquals(POST_response, 302)
+        self.assertEquals(POST_response['Location'],
+                          'http://%s%s' % (
+                self.site_location.site.domain,
+                self.url))
+
+        site_location = models.SiteLocation.objects.get(
+            pk=self.site_location.pk)
+        self.assertEquals(site_location.logo.name, logo_name)
+        self.assertEquals(site_location.background.name,
+                          background_name)
+
     def test_POST_delete_background(self):
         """
         A POST request to the edit_content view with POST['delete_background']
