35a85f94424955b1e627171cdb1751a627a65e49
Paul Swartz <paulswartz@gmail.com>
2010-10-01 16:16:27 -0400

    force older feeds to be visible on the admin page

--- localtv/management/commands/update_feeds.py
+++ localtv/management/commands/update_feeds.py
@@ -1,4 +1,5 @@
 import traceback
+import datetime
 
 from django.core.management.base import NoArgsCommand
 from localtv.management import site_too_old
@@ -11,6 +12,14 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         if site_too_old():
             return
+
+        # all feeds submitted more than an hour ago should be shown
+        hour = datetime.timedelta(hours=1)
+        models.Feed.objects.filter(
+            when_submitted__lte=datetime.datetime.now()-hour,
+            status=models.FEED_STATUS_UNAPPROVED).update(
+            status=models.FEED_STATUS_ACTIVE)
+
         for feed in models.Feed.objects.filter(
             status=models.FEED_STATUS_ACTIVE):
             try:
