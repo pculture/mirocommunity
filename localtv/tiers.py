d7a82cec4ed87976b58e0a125babbddf3749b5ed
Asheesh Laroia <asheesh@openhatch.org>
2010-11-15 14:02:14 -0500

    Adding trivial initial support for naming restrictions in the free tier.

---
+++ localtv/tiers.py
@@ -0,0 +1,14 @@
+class Tier(object):
+    def __init__(self, tier_name):
+        pass
+
+    def videos_limit(self):
+        return 500
+
+    def admins_limit(self):
+        return 1
+
+    def permit_custom_css(self):
+        return False
+
+                    
