5e06f177a1f7552de4476f8e7358e9374e8303db
Asheesh Laroia <asheesh@openhatch.org>
2010-11-19 13:07:29 -0500

    In example_project, make urls.py copyable.

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls.defaults import patterns, include
 
+def get_localtv_path(sub_path):
+    import os
+    import localtv
+    base = os.path.abspath(os.path.join(os.path.dirname(localtv.__file__), '..'))
+    return os.path.join(base, sub_path)
+
 urlpatterns = patterns('',
                        (r'^(?P<path>(?:css|images|js|swf|versioned).*)', 'django.views.static.serve',
-                        {'document_root': '../src/miro-community/static/'}),
+                        {'document_root': get_localtv_path('static')}),
                        (r'^localtv/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'localtv'}),
                        (r'^uploadtemplate/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'uploadtemplate'}),
                        #(r'^openid/', include('localtv_openid.urls')),
