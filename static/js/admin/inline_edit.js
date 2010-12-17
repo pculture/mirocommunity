9efe193a9390f999f2701999ca60777a5bf05320
Asheesh Laroia <asheesh@openhatch.org>
2010-12-17 16:37:43 -0500

    Add some Javascript fixes that jslint suggested.

--- static/js/admin/inline_edit.js
+++ static/js/admin/inline_edit.js
@@ -1,6 +1,6 @@
 function insert_and_activate_action_buttons(obj) {
     if (!obj.children('.done').length) {
-        button_code = '<button type="submit" class="done"><span>Save Changes</span></button>'
+        button_code = '<button type="submit" class="done"><span>Save Changes</span></button>';
         if (obj.find('.checklist').length) {
             obj.prepend(button_code);
         } else {
