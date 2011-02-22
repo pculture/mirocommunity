62482b0dc7a8e69cd3aa587596c0f59bf056c5bf
Asheesh Laroia <asheesh@asheesh.org>
2011-02-22 11:51:47 -0500

    Moving functions around in place, so that it is easier to see which does what

--- localtv/admin/tiers.py
+++ localtv/admin/tiers.py
@@ -41,29 +41,56 @@ from localtv.admin import forms
 import localtv.tiers
 import localtv.paypal_snippet
 
-@csrf_exempt
-def paypal_return(request):
-    '''This view is where PayPal sends users to upon success. Some things to note:
+### Below this line
+### ----------------------------------------------------------------------
+### These are admin views that the user will see at /admin/*
 
-    * PayPal sends us an "auth" parameter that we cannot validate.
-    * This should be a POST so that cross-site scripting can't just upgrade people's sites.
-    * This is not as secure as I would like.
+@require_site_admin
+@csrf_protect
+def confirmed_change_tier(request):
+    '''The point of this function is to provide somewhere for the PayPal form to POST
+    to -- instead of PayPal. So we start by asserting that PayPal is skipped, and then
+    we simply change the tier, and redirect back to the site level admin page.'''
+    skip_paypal = getattr(settings, "LOCALTV_SKIP_PAYPAL", False)
+    assert skip_paypal
 
-    Suggested improvements:
-    * The view that sends people to PayPal should store some state in the database
-      that this view checks. It only permits an upgrade in that situation.
-    * That could be the internal "payment_secret" to prevent CSRF.
-    * A tricky site admin could still try POST the right data to this view, which would
-      trigger the tier change.
+    target_tier_name = request.POST.get('target_tier_name', '')
 
-    If you want to exploit a MC site and change its tier, and you can cause an admin
-    with a cookie that's logged-in to visit pages you want, and you can get that admin
-    to do a POST, you still have to POST a value for the "auth" key. Note that this is
-    why we do a sanity-check of tier+payment status every night; we will catch funny
-    business within a day or so.'''
-    auth = request.POST.get('auth', None) or request.GET.get('auth', None)
-    if not auth:
-        return HttpResponseForbidden("You failed to submit an 'auth' token.")
+    # validate
+    if target_tier_name not in dict(localtv.tiers.CHOICES):
+        # Always redirect back to tiers page
+        return HttpResponseRedirect(reverse('localtv_admin_tier'))
+
+    return _actually_switch_tier(target_tier_name)
+
+@require_site_admin
+def downgrade_confirm(request):
+    target_tier_name = request.POST.get('target_tier_name', None)
+    # validate
+    if target_tier_name in dict(localtv.tiers.CHOICES):
+        target_tier_obj = localtv.tiers.Tier(target_tier_name)
+
+        would_lose = localtv.tiers.user_warnings_for_downgrade(target_tier_name)
+        data = {}
+        data['tier_name'] = target_tier_name
+        data['paypal_sandbox'] = getattr(settings, 'PAYPAL_TEST', False)
+        p = localtv.paypal_snippet.PayPal.get_with_django_settings()
+        data['paypal_url'] = p.PAYPAL_URL
+        data['paypal_email'] = getattr(settings, 'PAYPAL_RECEIVER_EMAIL', '')
+        data['target_tier_obj'] = target_tier_obj
+        data['would_lose_admin_usernames'] = localtv.tiers.push_number_of_admins_down(target_tier_obj.admins_limit())
+        data['customtheme_nag'] = ('customtheme' in would_lose)
+        data['advertising_nag'] = ('advertising' in would_lose)
+        data['customdomain_nag'] = ('customdomain' in would_lose)
+        data['css_nag'] = ('css' in would_lose)
+        data['videos_nag'] = ('videos' in would_lose)
+        data['videos_over_limit'] = localtv.tiers.hide_videos_above_limit(target_tier_obj)
+        data['new_theme_name'] = localtv.tiers.switch_to_a_bundled_theme_if_necessary(target_tier_obj)
+        data['payment_secret'] = request.tier_info.get_payment_secret()
+        return render_to_response('localtv/admin/downgrade_confirm.html', data,
+                                  context_instance=RequestContext(request))
+
+    # In some weird error case, redirect back to tiers page
     return HttpResponseRedirect(reverse('localtv_admin_tier'))
 
 @require_site_admin
@@ -104,6 +131,91 @@ def upgrade(request):
 
     return render_to_response('localtv/admin/upgrade.html', data,
                               context_instance=RequestContext(request))
+
+### Below this line:
+### -------------------------------------------------------------------------
+### These functions are resquest handlers that actually switch tier.
+
+@csrf_exempt
+def paypal_return(request):
+    '''This view is where PayPal sends users to upon success. Some things to note:
+
+    * PayPal sends us an "auth" parameter that we cannot validate.
+    * This should be a POST so that cross-site scripting can't just upgrade people's sites.
+    * This is not as secure as I would like.
+
+    Suggested improvements:
+    * The view that sends people to PayPal should store some state in the database
+      that this view checks. It only permits an upgrade in that situation.
+    * That could be the internal "payment_secret" to prevent CSRF.
+    * A tricky site admin could still try POST the right data to this view, which would
+      trigger the tier change.
+
+    If you want to exploit a MC site and change its tier, and you can cause an admin
+    with a cookie that's logged-in to visit pages you want, and you can get that admin
+    to do a POST, you still have to POST a value for the "auth" key. Note that this is
+    why we do a sanity-check of tier+payment status every night; we will catch funny
+    business within a day or so.'''
+    auth = request.POST.get('auth', None) or request.GET.get('auth', None)
+    if not auth:
+        return HttpResponseForbidden("You failed to submit an 'auth' token.")
+    return HttpResponseRedirect(reverse('localtv_admin_tier'))
+
+@csrf_exempt
+@require_site_admin
+def begin_free_trial(request, payment_secret):
+    '''This is where PayPal sends the user, if they are going to begin a free trial.
+
+    At this stage, we do not know what tier the user wanted to opt into. That should be stored
+    in the ?target_tier_name=... GET parameter.
+
+    If it is some nonsense, we should show an obscure error message and tell them to email
+    questions@MC if they got it.
+
+    If it what we expect, then:
+
+    * For now, trust that the IPN process will happen in the background,
+
+    * Declare the free trial in-use, and
+
+    * Switch the tier.'''
+    if payment_secret != request.tier_info.payment_secret:
+        raise HttpResponseForbidden("You are accessing this URL with invalid parameters. If you think you are seeing this message in error, email questions@mirocommunity.org")
+    target_tier_name = request.GET.get('target_tier_name', '')
+    if target_tier_name not in dict(localtv.tiers.CHOICES):
+        return HttpResponse("Something went wrong switching your site level. Please send an email to questions@mirocommunity.org immediately.")
+
+    # This is so that we can detect sites that start a free trial, but never generate
+    # the IPN event.
+    if request.tier_info.free_trial_started_on is None:
+        request.tier_info.free_trial_started_on = datetime.datetime.utcnow()
+
+    # Set the free trial to be in-use.
+    if request.tier_info.free_trial_available:
+        request.tier_info.free_trial_available = False
+        request.tier_info.save()
+
+    # Switch the tier!
+    return _actually_switch_tier(request, target_tier_name)
+
+### Below this line
+### --------------------------------------------------------------------------------------------
+### This function is something PayPal POSTs updates to.
+
+@csrf_exempt
+def ipn_endpoint(request, payment_secret):
+    # PayPal sends data to this function via POST.
+    #
+    # At this point in processing, the data might be fake. Let's pass it to
+    # the django-paypal code and ask it to verify it for us.
+    if payment_secret == request.tier_info.payment_secret:
+        response = paypal.standard.ipn.views.ipn(request)
+        return response
+    return HttpResponseForbidden("You submitted something invalid to this IPN handler.")
+
+### Below this line
+### ----------------------------------------------------------------------
+### These are helper functions.
 
 def _create_recurring_payment(request, token, amount, startdate):
     p = localtv.paypal_snippet.PayPal(
@@ -172,65 +284,6 @@ def _actually_switch_tier(request, target_tier_name):
     # Always redirect back to tiers page
     return HttpResponseRedirect(reverse('localtv_admin_tier'))
 
-@require_site_admin
-@csrf_protect
-def confirmed_change_tier(request):
-    '''The point of this function is to provide somewhere for the PayPal form to POST
-    to -- instead of PayPal. So we start by asserting that PayPal is skipped, and then
-    we simply change the tier, and redirect back to the site level admin page.'''
-    skip_paypal = getattr(settings, "LOCALTV_SKIP_PAYPAL", False)
-    assert skip_paypal
-
-    target_tier_name = request.POST.get('target_tier_name', '')
-
-    # validate
-    if target_tier_name not in dict(localtv.tiers.CHOICES):
-        # Always redirect back to tiers page
-        return HttpResponseRedirect(reverse('localtv_admin_tier'))
-
-    return _actually_switch_tier(target_tier_name)
-
-@require_site_admin
-def downgrade_confirm(request):
-    target_tier_name = request.POST.get('target_tier_name', None)
-    # validate
-    if target_tier_name in dict(localtv.tiers.CHOICES):
-        target_tier_obj = localtv.tiers.Tier(target_tier_name)
-
-        would_lose = localtv.tiers.user_warnings_for_downgrade(target_tier_name)
-        data = {}
-        data['tier_name'] = target_tier_name
-        data['paypal_sandbox'] = getattr(settings, 'PAYPAL_TEST', False)
-        p = localtv.paypal_snippet.PayPal.get_with_django_settings()
-        data['paypal_url'] = p.PAYPAL_URL
-        data['paypal_email'] = getattr(settings, 'PAYPAL_RECEIVER_EMAIL', '')
-        data['target_tier_obj'] = target_tier_obj
-        data['would_lose_admin_usernames'] = localtv.tiers.push_number_of_admins_down(target_tier_obj.admins_limit())
-        data['customtheme_nag'] = ('customtheme' in would_lose)
-        data['advertising_nag'] = ('advertising' in would_lose)
-        data['customdomain_nag'] = ('customdomain' in would_lose)
-        data['css_nag'] = ('css' in would_lose)
-        data['videos_nag'] = ('videos' in would_lose)
-        data['videos_over_limit'] = localtv.tiers.hide_videos_above_limit(target_tier_obj)
-        data['new_theme_name'] = localtv.tiers.switch_to_a_bundled_theme_if_necessary(target_tier_obj)
-        data['payment_secret'] = request.tier_info.get_payment_secret()
-        return render_to_response('localtv/admin/downgrade_confirm.html', data,
-                                  context_instance=RequestContext(request))
-            
-    # In some weird error case, redirect back to tiers page
-    return HttpResponseRedirect(reverse('localtv_admin_tier'))
-
-@csrf_exempt
-def ipn_endpoint(request, payment_secret):
-    # PayPal sends data to this function via POST.
-    #
-    # At this point in processing, the data might be fake. Let's pass it to
-    # the django-paypal code and ask it to verify it for us.
-    if payment_secret == request.tier_info.payment_secret:
-        response = paypal.standard.ipn.views.ipn(request)
-        return response
-    return HttpResponseForbidden("You submitted something invalid to this IPN handler.")
-
 from paypal.standard.ipn.signals import subscription_signup, subscription_cancel, subscription_eot, subscription_modify
 def handle_recurring_profile_start(sender, **kwargs):
     ipn_obj = sender
@@ -276,40 +329,3 @@ def on_subscription_cancel_switch_to_basic(sender, **kwargs):
 subscription_cancel.connect(on_subscription_cancel_switch_to_basic)
 subscription_eot.connect(on_subscription_cancel_switch_to_basic)
 subscription_modify.connect(handle_recurring_profile_start)
-
-@csrf_exempt
-@require_site_admin
-def begin_free_trial(request, payment_secret):
-    '''This is where PayPal sends the user, if they are going to begin a free trial.
-
-    At this stage, we do not know what tier the user wanted to opt into. That should be stored
-    in the ?target_tier_name=... GET parameter.
-
-    If it is some nonsense, we should show an obscure error message and tell them to email
-    questions@MC if they got it.
-
-    If it what we expect, then:
-
-    * For now, trust that the IPN process will happen in the background,
-
-    * Declare the free trial in-use, and
-
-    * Switch the tier.'''
-    if payment_secret != request.tier_info.payment_secret:
-        raise HttpResponseForbidden("You are accessing this URL with invalid parameters. If you think you are seeing this message in error, email questions@mirocommunity.org")
-    target_tier_name = request.GET.get('target_tier_name', '')
-    if target_tier_name not in dict(localtv.tiers.CHOICES):
-        return HttpResponse("Something went wrong switching your site level. Please send an email to questions@mirocommunity.org immediately.")
-
-    # This is so that we can detect sites that start a free trial, but never generate
-    # the IPN event.
-    if request.tier_info.free_trial_started_on is None:
-        request.tier_info.free_trial_started_on = datetime.datetime.utcnow()
-
-    # Set the free trial to be in-use.
-    if request.tier_info.free_trial_available:
-        request.tier_info.free_trial_available = False
-        request.tier_info.save()
-
-    # Switch the tier!
-    return _actually_switch_tier(request, target_tier_name)
