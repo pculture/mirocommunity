d6b1029f484fbc8163c96dfd63d0c2ee1d977374
Asheesh Laroia <asheesh@asheesh.org>
2011-03-28 21:14:40 -0400

    send_tiers_compliance_email now sends you a note even if you are within your tier

--- localtv/management/commands/send_tiers_compliance_email.py
+++ localtv/management/commands/send_tiers_compliance_email.py
@@ -29,15 +29,14 @@ class Command(BaseCommand):
 
         sitelocation = localtv.models.SiteLocation.objects.get_current()
         warnings = localtv.tiers.user_warnings_for_downgrade(sitelocation.tier_name)
-        if warnings:
-            data = {'warnings': warnings}
-            data['would_lose_admin_usernames'] = localtv.tiers.push_number_of_admins_down(target_tier_obj.admins_limit())
-            data['videos_over_limit'] = localtv.tiers.hide_videos_above_limit(target_tier_obj)
-            data['video_count'] = localtv.tiers.current_videos_that_count_toward_limit().count()
-            localtv.tiers.send_tiers_related_email(
-                'Changes in your Miro Community site',
-                'localtv/admin/tiers_emails/too_big_for_your_tier.txt',
-                sitelocation,
-                extra_context=data)
-            ti.already_sent_tiers_compliance_email = True
-            ti.save()
+        data = {'warnings': warnings}
+        data['would_lose_admin_usernames'] = localtv.tiers.push_number_of_admins_down(sitelocation.get_tier().admins_limit())
+        data['videos_over_limit'] = localtv.tiers.hide_videos_above_limit(sitelocation.get_tier())
+        data['video_count'] = localtv.tiers.current_videos_that_count_toward_limit().count()
+        localtv.tiers.send_tiers_related_email(
+            'Changes in your Miro Community site',
+            'localtv/admin/tiers_emails/too_big_for_your_tier.txt',
+            sitelocation,
+            extra_context=data)
+        ti.already_sent_tiers_compliance_email = True
+        ti.save()
