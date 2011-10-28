14f33c9bd6ce9c569cab8a95f0d99bdbc8c26832
Stephen Burrows <stephen.r.burrows@gmail.com>
2011-10-28 13:46:08 -0700

    Tentative movement towards a 'moderation' admin section.

---
+++ localtv/admin/moderation/videos.py
@@ -0,0 +1,58 @@
+# Copyright 2009 - Participatory Culture Foundation
+# 
+# This file is part of Miro Community.
+# 
+# Miro Community is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# Miro Community is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with Miro Community.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.contrib.sites.models import Site
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView
+
+from localtv.admin.feeds import generate_secret
+from localtv.decorators import require_site_admin
+from localtv.models import Video
+
+
+class VideoReviewView(ListView):
+    paginate_by = 10
+    queryset = Video.objects.filter(
+        status=Video.UNAPPROVED,
+        site=Site.objects.get_current()
+    ).order_by('when_submitted', 'when_published')
+    context_object_name = "video_list"
+    template_name = 'localtv/admin/videos/review.html'
+
+    @method_decorator(require_site_admin)
+    def dispatch(self, *args, **kwargs):
+        super(VideoReviewView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(VideoReviewView, self).get_context_data(**kwargs)
+        try:
+            current_video = context['object_list'][0]
+        except IndexError:
+            current_video = None
+        
+        context.update({
+            'feed_secret': generate_secret(),
+            'current_video': current_video
+        })
+        return context
+
+
+class VideoPreviewView(DetailView):
+    queryset = Video.objects.filter(
+        status=Video.UNAPPROVED,
+        site=Site.objects.get_current()
+    )
