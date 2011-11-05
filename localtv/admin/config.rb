04cce5f35bc324df2ac6ea40560c614e24be1c62
Stephen Burrows <stephen.r.burrows@gmail.com>
2011-11-04 17:11:54 -0700

    Added initial sass setup to localtv/admin.

---
+++ localtv/admin/config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "static/localtv/admin/css"
+sass_dir = "sass"
+images_dir = "static/localtv/admin/images"
+javascripts_dir = "static/localtv/admin/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
