7ae62eccd9043d22d88dd4647e0ac35e58156948
Stephen Burrows <stephen.r.burrows@gmail.com>
2011-12-02 20:37:56 +0100

    Corrected spacing in source views.py.

--- localtv/admin/sources/views.py
+++ localtv/admin/sources/views.py
@@ -16,20 +16,19 @@
 # along with Miro Community.  If not, see <http://www.gnu.org/licenses/>.
 
 from localtv.admin.views import MiroCommunityAdminCreateView
-from localtv.models import Feed
 from localtv.tasks import CELERY_USING
 
 
 class SourceCreateView(MiroCommunityAdminCreateView):
-	"""
-	View for creating sources, which immediately queues a task for importing
-	the source.
+    """
+    View for creating sources, which immediately queues a task for importing
+    the source.
 
-	"""
-	#: The task for importing the source. Default: None.
-	import_task = None
+    """
+    #: The task for importing the source. Default: None.
+    import_task = None
 
-	def form_valid(self, form):
-		response = super(SourceCreateView, self).form_valid(form)
-		self.import_task.delay(self.object.pk, using=CELERY_USING)
-		return response
+    def form_valid(self, form):
+        response = super(SourceCreateView, self).form_valid(form)
+        self.import_task.delay(self.object.pk, using=CELERY_USING)
+        return response
