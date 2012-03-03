3b0a65ac2033ede213de3087e90a1973a0f7db8f
Harris Lapiroff <harris@chromamine.com>
2012-03-03 18:25:36 -0500

    Better dropdown behavior.

--- localtv/static/localtv/front/js/localtv.dropdown.js
+++ localtv/static/localtv/front/js/localtv.dropdown.js
@@ -1,18 +1,23 @@
 $(function(){
 	var toggle_dropdown = function (e) {
-		var $this = $(this),
-			dropdown_parent = $this.parent();
-			dropdown_target = dropdown_parent.children('[data-dropdown-target]');
-		if($this.data('open')){
-			dropdown_target.hide()
-			dropdown_parent.removeClass('open')
-			$this.data('open', false)
-		}else{
-			dropdown_target.show()
-			dropdown_parent.addClass('open')
-			$this.data('open', true)
-		}
-		e.preventDefault()
-	};
+			var $this = $(this),
+				dropdown_parent = $this.parent();
+				dropdown_target = dropdown_parent.children('[data-dropdown-target]');
+			if($this.attr('data-dropdown-open')==='true'){
+				dropdown_target.hide()
+				dropdown_parent.removeClass('open')
+				$this.attr('data-dropdown-open', 'false')
+			}else{
+				dropdown_target.show()
+				dropdown_parent.addClass('open')
+				$this.attr('data-dropdown-open', 'true')
+			}
+			e.preventDefault();
+			e.stopPropagation();
+		},
+		close_dropdowns = function (e){
+			$('[data-dropdown-open="true"]').click()
+		};
+	$(document.body).click(close_dropdowns)
 	$('[data-dropdown] > a').on('click', toggle_dropdown);
 })
