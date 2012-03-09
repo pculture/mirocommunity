dcb7117facd029b173c7b371045ab717f9719e8f
Harris Lapiroff <harris@chromamine.com>
2012-03-08 23:36:57 -0500

    Updates to the navigation.

--- localtv/static/localtv/front/js/localtv.dropdown.js
+++ localtv/static/localtv/front/js/localtv.dropdown.js
@@ -1,23 +1,36 @@
 $(function(){
-	var toggle_dropdown = function (e) {
+	var close_dropdown = function (dropdown_target, dropdown_parent) {
 			var $this = $(this),
 				dropdown_parent = $this.parent();
 				dropdown_target = dropdown_parent.children('[data-dropdown-target]');
+			dropdown_target.hide();
+			dropdown_parent.removeClass('open');
+			$this.attr('data-dropdown-open', 'false');
+		},
+		open_dropdown = function (dropdown_target, dropdown_parent) {
+			var $this = $(this),
+				dropdown_parent = $this.parent();
+				dropdown_target = dropdown_parent.children('[data-dropdown-target]');
+			dropdown_target.show();
+			dropdown_parent.addClass('open');
+			$this.attr('data-dropdown-open', 'true');
+		},
+		toggle_dropdown = function (e) {
+			var $this = $(this);
 			if($this.attr('data-dropdown-open')==='true'){
-				dropdown_target.hide()
-				dropdown_parent.removeClass('open')
-				$this.attr('data-dropdown-open', 'false')
+				// if the dropdown menu is already open, close it
+				close_dropdown.call(this);
 			}else{
-				dropdown_target.show()
-				dropdown_parent.addClass('open')
-				$this.attr('data-dropdown-open', 'true')
+				// if the dropdown menu is closed, open it
+				close_dropdowns(); // close other dropdowns
+				open_dropdown.call(this);
 			}
 			e.preventDefault();
 			e.stopPropagation();
 		},
 		close_dropdowns = function (e){
-			$('[data-dropdown-open="true"]').click()
+			$('[data-dropdown-open="true"]').click();
 		};
-	$(document.body).click(close_dropdowns)
+	$(document.body).click(close_dropdowns);
 	$('[data-dropdown] > a').on('click', toggle_dropdown);
 })
