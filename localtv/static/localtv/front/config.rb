895136fb63ae5d9064048b3674a7b4e8ab6cd815
Harris Lapiroff <harris@chromamine.com>
2012-03-14 22:53:23 -0400

    Restructured the video detail page and added a texture to pre-content divs.

--- localtv/static/localtv/front/config.rb
+++ localtv/static/localtv/front/config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/static/localtv/front/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
