0d93ef1c00b8e912e5b30e2703ecbd582d3d9bf3
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-03-28 01:55:29 -0700

    Added --since support to update_popularity command.

--- localtv/management/commands/update_popularity.py
+++ localtv/management/commands/update_popularity.py
@@ -16,22 +16,29 @@
 # along with Miro Community. If not, see <http://www.gnu.org/licenses/>.
 
 from datetime import datetime, timedelta
+from optparse import make_option
 
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import LabelCommand
 
 from localtv.management import site_too_old
 from localtv.models import Video
 
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, verbosity=0, **options):
+class Command(LabelCommand):
+    option_list = (
+        make_option('--since', action='store', dest='since', default=11,
+                    type='int', help='The number of days in the past for which all watched videos should be reindexed.'),
+    ) + LabelCommand.option_list
+
+    def handle(self, **options):
         if site_too_old():
             return
 
+        since = options['since']
         from localtv.tasks import haystack_batch_update, CELERY_USING
 
         haystack_batch_update.delay(Video._meta.app_label,
                                     Video._meta.module_name,
-                                    start=datetime.now() - timedelta(11),
+                                    start=datetime.now() - timedelta(since),
                                     date_lookup='watch__timestamp',
                                     using=CELERY_USING)
