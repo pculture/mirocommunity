7198bc84573f60d58aae77a5f849c2efeb0951a0
Harris Lapiroff <harris@chromamine.com>
2012-04-17 02:50:14 -0400

    Added popovers on video thumbnails.

--- localtv/static/localtv/front/js/localtv.kickoff.js
+++ localtv/static/localtv/front/js/localtv.kickoff.js
@@ -3,4 +3,6 @@
 	$('[data-cycle]').cycle({fx: 'scrollUp'});
 	// shrinkydink all .video-details classes
 	$('.video-details').shrinkydink();
+	// add popovers to the video thumbs
+	$('.video-grid-item').popover({delay:{show: 250, hide: 0}});
 });
