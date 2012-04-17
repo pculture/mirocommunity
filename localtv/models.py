bf2d0253649b284ac6e5d2a91ce37041ac06842d
Paul Swartz <paulswartz@gmail.com>
2012-04-17 15:03:27 -0400

    removed a bunch of stuff that's now in the SAAS repo
    A bunch of tests are going to fail; I doubt this branch is even runnable.

--- localtv/models.py
+++ localtv/models.py
@@ -23,7 +23,6 @@ import re
 import urllib
 import urllib2
 import mimetypes
-import base64
 import os
 import logging
 import sys
@@ -230,67 +229,6 @@ class SingletonManager(models.Manager):
         return singleton
 
 
-class TierInfo(models.Model):
-    payment_due_date = models.DateTimeField(null=True, blank=True)
-    free_trial_available = models.BooleanField(default=True)
-    free_trial_started_on = models.DateTimeField(null=True, blank=True)
-    in_free_trial = models.BooleanField(default=False)
-    payment_secret = models.CharField(max_length=255, default='',blank=True) # This is part of payment URLs.
-    current_paypal_profile_id = models.CharField(max_length=255, default='',blank=True) # NOTE: When using this, fill it if it seems blank.
-    video_allotment_warning_sent = models.BooleanField(default=False)
-    free_trial_warning_sent = models.BooleanField(default=False)
-    already_sent_welcome_email = models.BooleanField(default=False)
-    inactive_site_warning_sent = models.BooleanField(default=False)
-    user_has_successfully_performed_a_paypal_transaction = models.BooleanField(default=False)
-    already_sent_tiers_compliance_email = models.BooleanField(default=False)
-    fully_confirmed_tier_name = models.CharField(max_length=255, default='', blank=True)
-    should_send_welcome_email_on_paypal_event = models.BooleanField(default=False)
-    waiting_on_payment_until = models.DateTimeField(null=True, blank=True)
-    site_settings = models.OneToOneField('SiteSettings', db_column='sitelocation_id')
-    objects = SingletonManager()
-
-    def get_payment_secret(self):
-        '''The secret had better be non-empty. So we make it non-empty right here.'''
-        if self.payment_secret:
-            return self.payment_secret
-        # Guess we had better fill it.
-        self.payment_secret = base64.b64encode(os.urandom(16))
-        self.save()
-        return self.payment_secret
-
-    def site_is_subsidized(self):
-        return (self.current_paypal_profile_id == 'subsidized')
-
-    def set_to_subsidized(self):
-        if self.current_paypal_profile_id:
-            raise AssertionError, (
-                "Bailing out: " +
-                "the site already has a payment profile configured: %s" %
-                                   self.current_paypal_profile_id)
-        self.current_paypal_profile_id = 'subsidized'
-
-    def time_until_free_trial_expires(self, now = None):
-        if not self.in_free_trial:
-            return None
-        if not self.payment_due_date:
-            return None
-
-        if now is None:
-            now = datetime.datetime.utcnow()
-        return (self.payment_due_date - now)
-
-    def use_zendesk(self):
-        '''If the site is configured to, we can send notifications of
-        tiers-related changes to ZenDesk, the customer support ticketing
-        system used by PCF.
-
-        A non-PCF deployment of localtv would not want to set the
-        LOCALTV_USE_ZENDESK setting. Then this method will return False,
-        and the parts of the tiers system that check it will avoid
-        making calls out to ZenDesk.'''
-        return lsettings.USE_ZENDESK
-
-
 class SiteSettings(Thumbnailable):
     """
     An extension to the django.contrib.sites site model, providing
@@ -381,27 +319,6 @@ class SiteSettings(Thumbnailable):
         self._queued_mail = []
         return ret
 
-    @staticmethod
-    def enforce_tiers(override_setting=None, using='default'):
-        '''If the admin has set LOCALTV_DISABLE_TIERS_ENFORCEMENT to a True value,
-        then this function returns False. Otherwise, it returns True.'''
-        if override_setting is None:
-            disabled = lsettings.DISABLE_TIERS_ENFORCEMENT
-        else:
-            disabled = override_setting
-
-        if disabled:
-            # Well, hmm. If the site admin participated in a PayPal transaction, then we
-            # actually will enforce the tiers.
-            #
-            # Go figure.
-            tierdata = TierInfo.objects.db_manager(using).get_current()
-            if tierdata.user_has_successfully_performed_a_paypal_transaction:
-                return True # enforce it.
-
-        # Generally, we just negate the "disabled" boolean.
-        return not disabled
-
     def user_is_admin(self, user):
         """
         Return True if the given User is an admin for this SiteSettings.
@@ -420,14 +337,6 @@ class SiteSettings(Thumbnailable):
 
     def get_tier(self):
         return localtv.tiers.Tier(self.tier_name, self)
-
-    def get_fully_confirmed_tier(self):
-        # If we are in a transitional state, then we would have stored
-        # the last fully confirmed tier name in an unusual column.
-        tierdata = TierInfo.objects.get_current()
-        if tierdata.fully_confirmed_tier_name:
-            return localtv.tiers.Tier(tierdata.fully_confirmed_tier_name)
-        return None
 
     def get_css_for_display_if_permitted(self):
         '''This function checks the site tier, and if permitted, returns the
