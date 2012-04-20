a4e3d849dcbfc19017879c288782aaf97669751c
Harris Lapiroff <harris@chromamine.com>
2012-04-20 01:16:50 -0400

    Updates to the carousel.

--- localtv/static/localtv/front/js/localtv.kickoff.js
+++ localtv/static/localtv/front/js/localtv.kickoff.js
@@ -1,6 +1,16 @@
 ;jQuery(function($){
 	// start anything which has a [data-cycle] attribute cycling
-	$('[data-cycle]').cycle({fx: 'scrollUp'});
+	$('.carousel').each(function(){
+		var $this = $(this),
+			pager = $('<div class="carousel-pager"></div>').appendTo($this.parent());
+		$this.cycle({
+			fx: 'scrollUp',
+			pause: true, // pause on hover
+			timeout: 6000, // 6 seconds between slides
+			pager: pager,
+			pauseOnPagerHover: true
+		});
+	});
 	// shrinkydink all .video-details classes
 	$('.video-details').shrinkydink();
 	// add popovers to the video thumbs
