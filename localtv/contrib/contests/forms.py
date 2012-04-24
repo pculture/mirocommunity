276ebcc9afabd9507cd939f9e7975850ccdf648f
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-04-24 14:35:04 -0700

    Added special handling for videos in the contest admin.

--- localtv/contrib/contests/forms.py
+++ localtv/contrib/contests/forms.py
@@ -16,24 +16,35 @@
 # along with Miro Community.  If not, see <http://www.gnu.org/licenses/>.
 
 from django import forms
+from django.conf import settings
 from django.contrib.sites.models import Site
 
-from localtv.contrib.contests.models import Contest
+from localtv.contrib.contests.models import Contest, ContestVideo
+from localtv.models import Video
 
 
 class ContestAdminForm(forms.ModelForm):
     detail_columns = forms.MultipleChoiceField(
                                         choices=Contest.DETAIL_COLUMN_CHOICES,
                                         widget=forms.CheckboxSelectMultiple)
+    videos = forms.ModelMultipleChoiceField(
+                            queryset=Video.objects.filter(status=Video.ACTIVE,
+                                                          site=settings.SITE_ID))
 
     class Meta:
         model = Contest
-        exclude = ('videos', 'site', 'detail_columns')
+        # Using fields until https://code.djangoproject.com/ticket/12337
+        # is resolved.
+        #exclude = ('videos', 'site', 'detail_columns')
+        fields = ('name', 'description', 'votes_per_user', 'allow_downvotes',
+                  'submissions_open', 'voting_open', 'display_vote_counts')
 
     def __init__(self, *args, **kwargs):
         super(ContestAdminForm, self).__init__(*args, **kwargs)
         self.initial['detail_columns'] = self.instance.detail_columns.split(
                                                                           ',')
+        if self.instance.pk:
+            self.initial['videos'] = self.instance.videos.all()
 
     def clean_detail_columns(self):
         return ','.join(self.cleaned_data['detail_columns'])
@@ -45,3 +56,23 @@ class ContestAdminForm(forms.ModelForm):
             self.instance.site = Site.objects.get_current()
 
         self.instance.detail_columns = self.cleaned_data['detail_columns']
+
+    def save(self, commit=True):
+        instance = super(ContestAdminForm, self).save(commit)
+        def save_m2m():
+            current_pks = set(instance.videos.values_list('pk', flat=True))
+            new_pks = set((v.pk for v in self.cleaned_data['videos']))
+            remove_pks = current_pks - new_pks
+            add_pks = new_pks - current_pks
+
+            ContestVideo.objects.filter(contest=instance,
+                                        video__in=remove_pks).delete()
+            ContestVideo.objects.bulk_create([
+                ContestVideo(video_id=pk, contest=instance)
+                for pk in add_pks])
+
+        if commit:
+            save_m2m()
+        else:
+            self.save_m2m = save_m2m
+        return instance
