3cb06b2095614d243e653437de57eefd40bc6bd8
Paul Swartz <paulswartz@gmail.com>
2012-05-08 13:41:25 -0400

    bz19082. try testing with ElasticSearch on Travis

--- example_mc_project/settings.py
+++ example_mc_project/settings.py
@@ -23,6 +23,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 USE_SOUTH = bool(os.environ.get('MC_TEST_USE_SOUTH', False))
+USE_ES = bool(os.environ.get('MC_TEST_USE_ES', False))
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
@@ -259,12 +260,21 @@ ACCOUNT_ACTIVATION_DAYS = 7
 FORCE_LOWERCASE_TAGS = True
 
 # haystack search
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-    }
-}
+if USE_ES:
+    HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+            'URL': 'http://localhost:9200/',
+            'INDEX_NAME': 'mirocommunity'
+            }
+        }
+else:
+    HAYSTACK_CONNECTIONS = {
+        'default': {
+            'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+            'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+            }
+        }
 
 # Facebook options
 FACEBOOK_APP_ID = None
