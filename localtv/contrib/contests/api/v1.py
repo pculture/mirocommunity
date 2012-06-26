fa37030d48c91610210f18df3141f7b0303533a8
Harris Lapiroff <harris@chromamine.com>
2012-06-26 02:30:00 -0400

    Contest API now spits out parseable errors when a vote fails.

--- localtv/contrib/contests/api/v1.py
+++ localtv/contrib/contests/api/v1.py
@@ -18,10 +18,11 @@
 from django.conf import settings
 
 from tastypie import fields
+from tastypie import http
 from tastypie.constants import ALL_WITH_RELATIONS
 from tastypie.exceptions import ImmediateHttpResponse
-from tastypie.http import HttpUnauthorized
 from tastypie.resources import ModelResource
+from tastypie.utils.mime import build_content_type
 
 from localtv.api.v1 import api, UserResource, VideoResource
 from localtv.contrib.contests.authorization import UserAuthorization
@@ -60,42 +61,53 @@ class ContestVoteResource(ModelResource):
     user = fields.ToOneField(UserResource, 'user')
     vote = fields.IntegerField('vote')
 
+    def _handle_error(self, request, error_message, response_class=http.HttpForbidden):
+        data = {
+            "error_message": error_message,
+        }
+        desired_format = self.determine_format(request)
+        serialized = self.serialize(request, data, desired_format)
+        response = response_class(content=serialized,
+                            content_type=build_content_type(desired_format))
+        raise ImmediateHttpResponse(response)
+
     def obj_create(self, bundle, request=None, **kwargs):
         """
         Creates a contest-vote object after ensuring that the user is set to
         the currently logged-in user, that the vote they are registering is
         allowed, and that they have not exceeded their vote count.
-        
+
         """
-        
-        # Verify authenticated user.
-        if hasattr(request, 'user') and request.user.is_authenticated():
-            kwargs['user'] = request.user
-        else:
-            raise ImmediateHttpResponse(response=HttpUnauthorized('User not authenticated.'))
-        
+
         # Extract relevant data.
         contestvideo = ContestVideoResource().get_via_uri(bundle.data['contestvideo'])
         contest = contestvideo.contest
         video = contestvideo.video
         vote_value = int(bundle.data['vote'])
-        
+
+        # Verify authenticated user.
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            kwargs['user'] = request.user
+        else:
+            self._handle_error(request,
+                               'User must be logged-in to vote.')
+
         # Check vote is permissible.
         permissible_votes = (1, -1) if contest.allow_downvotes else (1,)
         if vote_value not in permissible_votes:
-            raise ImmediateHttpResponse(
-                    response=HttpUnauthorized('Vote value %d not permitted.' %
-                    vote_value))
-        
+            self._handle_error(request,
+                               'Vote value %d not permitted.' % vote_value)
+
         # Check vote counts.
         if contest.votes_per_user is not None:
             user_vote_count = ContestVote.objects.filter(
                                                 contestvideo__contest=contest,
                                                 user=request.user).count()
             if user_vote_count + 1 > contest.votes_per_user:
-                raise ImmediateHttpResponse(
-                        response=HttpUnauthorized('Maximum votes exceeded.'))
-    
+                self._handle_error(request,
+                                   'Users may only vote %d times in %s.' %
+                                   (contest.votes_per_user, contest.name))
+
         # Since everything checks out, go ahead and create the vote.
         return super(ContestVoteResource, self).obj_create(bundle,
                                                            request=request,
