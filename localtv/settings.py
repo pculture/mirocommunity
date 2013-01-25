e6ba7443b07e2d5163711b63d1fd6ec34c9bc025
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-01-24 17:45:29 -0800

    bz19810. Purged OriginalVideo functionality.

--- localtv/settings.py
+++ localtv/settings.py
@@ -4,9 +4,6 @@ from django.conf import settings
 #: Default: 2 hours. (2 * 60 * 60 seconds)
 POPULAR_QUERY_TIMEOUT =  getattr(settings, 'LOCALTV_POPULAR_QUERY_TIMEOUT',
                                  2 * 60 * 60)
-ENABLE_ORIGINAL_VIDEO = not getattr(settings,
-                                    'LOCALTV_DONT_LOG_REMOTE_VIDEO_HISTORY',
-                                    None)
 ENABLE_CHANGE_STAMPS = getattr(settings, 'LOCALTV_ENABLE_CHANGE_STAMPS', False)
 SHOW_ADMIN_DASHBOARD = getattr(settings, 'LOCALTV_SHOW_ADMIN_DASHBOARD', True)
 SHOW_ADMIN_ACCOUNT_LEVEL = getattr(settings, 'LOCALTV_SHOW_ADMIN_ACCOUNT_LEVEL',
