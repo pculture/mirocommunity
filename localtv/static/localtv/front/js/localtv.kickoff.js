138b65c2993caca21fb5a1f92236465841c6f0fa
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-02-18 16:45:07 -0800

    [Backported aad6052] bz19845. Fixed infinite scrolling issues.
    Conflicts:
    
    	localtv/templates/localtv/__base.html

--- localtv/static/localtv/front/js/localtv.kickoff.js
+++ localtv/static/localtv/front/js/localtv.kickoff.js
@@ -1,10 +1,5 @@
 ;jQuery(function($){
-	
-	// if a shebang is present in the URI, redirect
-	if (window.location.hash.search('#!') === 0) {
-		window.location = window.location.href.replace(/#!/g,'')
-	}
-	
+
 	// start anything which has a [data-cycle] attribute cycling
 	$('.carousel').each(function(){
 		var $this = $(this),
@@ -33,61 +28,41 @@
 		// distance from edge of hover element + width of hover element + width of popover
 		return (position.left + position.width + 300 > $(window).width()) ? 'left' : 'right';
 	}});
-	
+
 	// Infinite Scroll
 	// Only triggered if a `body.video-list-page` exists and we're on the first page
 	// TODO: handle the case where we are not on the first page.
-	if ($('body').hasClass('video-list-page') && $('.pagetabs').find('li:first').hasClass('selected')) {
+	// History!
+	var History = window.History;
+	if (History.enabled && $('body').hasClass('video-list-page') && $('.pagetabs').find('li:first').hasClass('selected')) {
+		History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+			var State = History.getState(); // Note: We are using History.getState() instead of event.state
+			History.log(State.data, State.title, State.url);
+		});
 		$('.pagetabs').hide(); // hide pagination
 		// preload the ajax loader image
 		var ajax_image = new Image();
 		ajax_image.src = STATIC_URL + "localtv/front/images/ajax-loader.gif";
 		// kick off the infinite scrolling
 		$('.grid').infinitescroll({
+			navSelector: '.pagetabs',
+			nextSelector: '.pagetabs > .selected + li > a',
+			itemSelector: '.grid-item',
 			loading: {
-				img: null,
+				msg: $("<li class=\"grid-item\" id=\"infscr-loading\">Loading&hellip;</li>"),
 				// override a lot of the default behavior
 				finished: function (opts) {
-					var $this = $(this);
-					window.location.hash = "#!?page=" + opts.state.currPage;
+					History.replaceState(null, null, "?page=" + opts.state.currPage);
 					$('#infscr-loading').remove(); // hide the spinner
-					$this.infinitescroll('pause'); // prevent from loading the next page
-					if ($this.data('infscr_first_load') !== 'done'){
-						// if this is the first load, create a next page button
-						$this.data('infscr_first_load', 'done');
-						var next_page_button =$('<a href="#" class="button button-wide">Load More Videos</a>');
-						$this.data('infscr_next_button', next_page_button);
-						next_page_button.click(function (e) {
-							$(this).hide();
-							$this.infinitescroll('resume');
-							e.preventDefault();
-						});
-						$this.after(next_page_button);
-					} else {
-						$this.data('infscr_next_button').show();
-					}
 				},
-				start: function (opts) {
-					opts.loading.msg = $("<li class=\"grid-item loading\" id=\"infscr-loading\">Loading&hellip;</li>")
-					$(opts.navSelector).hide();
-					opts.loading.msg
-						.appendTo(opts.loading.selector)
-						.show(opts.loading.speed, function () {
-							beginAjax(opts);
-						});
-				}
 			},
 			errorCallback: function (error) {
 				if(error === "done"){
 					var $infscrLoading = $('#infscr-loading');
-					$infscrLoading.after("<li class=\"grid-item done\" id=\"infscr-done\">End of Videos</li>");
+					$infscrLoading.after("<li class=\"padded\" id=\"infscr-done\">End of Videos</li>");
 					$infscrLoading.remove();
-					$this.data('infscr_next_button').remove();
 				}
-			},
-			navSelector: '.pagetabs',
-			nextSelector: '.pagetabs > .selected + li > a',
-			itemSelector: '.grid-item'
+			}
 		});
 	};
 	
