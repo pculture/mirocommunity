ced62dbc367c63ec4e04a293371bbc67fc6361f2
Stephen Burrows <stephen.r.burrows@gmail.com>
2013-04-01 21:19:30 -0700

    Added support for YOUTUBE_API_KEY setting. Also corrected API_KEYS to only include keys that are actually set.

--- localtv/settings.py
+++ localtv/settings.py
@@ -1,16 +1,24 @@
 from django.conf import settings
 
+__all__ = ('POPULAR_QUERY_TIMEOUT', 'SHOW_ADMIN_DASHBOARD',
+           'SHOW_ADMIN_ACCOUNT_LEVEL', 'USE_HAYSTACK', 'API_KEYS')
+
 #: The amount of time that the "popular videos" query is considered valid.
 #: Default: 2 hours. (2 * 60 * 60 seconds)
-POPULAR_QUERY_TIMEOUT =  getattr(settings, 'LOCALTV_POPULAR_QUERY_TIMEOUT',
-                                 2 * 60 * 60)
+POPULAR_QUERY_TIMEOUT = getattr(settings, 'LOCALTV_POPULAR_QUERY_TIMEOUT',
+                                2 * 60 * 60)
 SHOW_ADMIN_DASHBOARD = getattr(settings, 'LOCALTV_SHOW_ADMIN_DASHBOARD', True)
 SHOW_ADMIN_ACCOUNT_LEVEL = getattr(settings, 'LOCALTV_SHOW_ADMIN_ACCOUNT_LEVEL',
                                    True)
 USE_HAYSTACK = getattr(settings, 'LOCALTV_USE_HAYSTACK', True)
 
-API_KEYS = {
-    'vimeo_key': getattr(settings, 'VIMEO_API_KEY', None),
-    'vimeo_secret': getattr(settings, 'VIMEO_API_SECRET', None),
-    'ustream_key': getattr(settings, 'USTREAM_API_KEY', None)
+_keymap = {
+    'vimeo_key': 'VIMEO_API_KEY',
+    'vimeo_secret': 'VIMEO_API_SECRET',
+    'ustream_key': 'USTREAM_API_KEY',
+    'youtube_key': 'YOUTUBE_API_KEY',
 }
+
+API_KEYS = dict((k, getattr(settings, v))
+                for k, v in _keymap.iteritems()
+                if getattr(settings, v, None) is not None)
